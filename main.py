593c3d23c8617d141ec8f2524551b2ba5ddc6fcc
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2025-07-22 07:30:58 -0500

    details added

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 
 # Basic Function using indention 
-
+this_line = True
 
 
 def my_function():
   print("Hello from a function")
 
-# Basic for loop 
-# "x" is the thing you are on and "fruits" is the array or object. you tell the function what to to dith x int the for loop
 
+
+
+  # For loop and while loop 
+
+# Use for loops when you have a defined number of interations 
+# While loop to interate until a condition is met 
+
+
+
+# Basic "for" loop 
+# "x" is the thing you are on and "fruits" is the array or object. you tell the function what to to dith x int the for loop
 
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
 
 
+# basic "while" loop based on a variable 
+
+while this_line == True:
+  print(f"here is a formated string {fruits[1]}")  
+  break
+
+count = 5
+# counting up in a while loop 
+while count < 10:
+  print(f"here is a formated string {fruits[0]}")
+  count = count + 1  
 
