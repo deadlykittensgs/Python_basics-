a0e1752de76cdc9e180aaa94702b58845ade5f79
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2025-07-21 22:02:09 -0500

    added info

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# Basic Function using indention 
+
+
+
+def my_function():
+  print("Hello from a function")
+
+# Basic for loop 
+# "x" is the thing you are on and "fruits" is the array or object. you tell the function what to to dith x int the for loop
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+
